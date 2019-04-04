0067f84532aa2314b373906ad754b45b8752d75f
New@4a <supapat.ne@gmail.com>
2019-04-04 09:06:16 +0700

    mongo init local test

--- routes/employee.js
+++ routes/employee.js
@@ -5,12 +5,12 @@ var mongoose = require('mongoose');
 
 /* GET home page. */
 router.getEmployee = (req,res) => {
-	res.render('employee',{
-	  title: 'Employee'
-	});
-	// information.find(function(err,dbBooks){
-	// res.render('test',{title:"รายการหนังสือ",data:dbBooks})
+	// res.render('employee',{
+	//   title: 'Employee'
 	// });
+	information.find(function(err,dbBooks){
+	res.render('test',{title:"รายการหนังสือ",data:dbBooks})
+	});
 };
 router.postEmployee = (req,res) => {
 	res.send("You got in post "+ req.body.txtName);
